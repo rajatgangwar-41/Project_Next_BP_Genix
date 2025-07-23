6142e20433eacaeb576f1417f861ad46ef56fef4
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-07-23 23:02:19 +0530

    added upgrade button to change to pro plan

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,8 +4,6 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Montserrat } from "next/font/google"
-
-import { cn } from "@/lib/utils"
 import {
   Code,
   ImageIcon,
@@ -15,6 +13,9 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { FreeCounter } from "@/components/free-counter"
 
 const montserrat = Montserrat({
   weight: "600",
@@ -65,8 +66,12 @@ const routes = [
   },
 ]
 
-const Sidebar = () => {
-  const pathName = usePathname()
+interface SidebarProps {
+  apiLimitCount: number
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
+  const pathname = usePathname()
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -85,7 +90,7 @@ const Sidebar = () => {
               key={route.href}
               className={cn(
                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                pathName === route.href
+                pathname === route.href
                   ? "text-white bg-white/10"
                   : "text-zinc-400"
               )}
@@ -98,6 +103,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   )
 }
